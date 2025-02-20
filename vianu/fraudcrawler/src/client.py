e22b809c5383f5dba0548ac61231d6f705b0bb08
Nicolas Perez <nicoapg1@gmail.com>
2025-02-20 16:11:35 +0100

    Addition of classified client using openAI's API

--- vianu/fraudcrawler/src/client.py
+++ vianu/fraudcrawler/src/client.py
@@ -6,6 +6,7 @@ import pandas as pd
 from vianu.fraudcrawler.src.serpapi import SerpApiClient
 from vianu.fraudcrawler.src.zyteapi import ZyteAPIClient
 from vianu.fraudcrawler.src.processor import Processor
+from vianu.fraudcrawler.src.classify_suspicious_product import ClassifySuspiciousProduct
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,7 @@ class FraudCrawlerClient:
         self,
         serpapi_key: str,
         zyteapi_key: str,
+        openai_api_key: str,
         location: str = "Switzerland",
         max_retries: int = 3,
         retry_delay: int = 2,
@@ -36,6 +38,7 @@ class FraudCrawlerClient:
             api_key=zyteapi_key, max_retries=max_retries, retry_delay=retry_delay
         )
         self._processor = Processor(location=location)
+        self._classifier_suspiciousness= ClassifySuspiciousProduct(openai_api_key=openai_api_key)
 
     def run(self, search_term: str, num_results=10) -> pd.DataFrame:
         """Runs the pipeline steps: search, get product details, processes them, and returns a DataFrame.
@@ -65,6 +68,9 @@ class FraudCrawlerClient:
         if not processed:
             logger.warning("No products left after processing.")
             return pd.DataFrame()
+
+        # Analyze Relevance
+        processed = self._classifier_suspiciousness.classify_suspicious_products(processed)
 
         # Flatten the product data
         df = pd.json_normalize(processed)
